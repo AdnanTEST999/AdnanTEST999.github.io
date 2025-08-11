55f415687007cc215dac500502ad82799025ebdb
Testeruser22 <githubsecondacc2021@gmail.com>
2025-08-11 15:14:42 +0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/AdnanTEST',             // add this explicitly
+  base: '/',             // add this explicitly
   server: {
     host: "::",
     port: 8080,
